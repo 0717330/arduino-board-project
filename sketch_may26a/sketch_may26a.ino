838341f5c97f7ececc8f3a1dd08b63d5b5e07b4c
connor mcfadden <0717330@schoolsnet.act.edu.au>
2022-06-06 14:33:58 +0930

    working turning and dc motor

--- sketch_may26a/sketch_may26a.ino
+++ sketch_may26a/sketch_may26a.ino
@@ -3,10 +3,14 @@
 #include <SPI.h>
 #include <SD.h>
 
+
+
 // Real Time Clock (RTC)
 #include "RTClib.h"
 RTC_Millis rtc;     // Software Real Time Clock (RTC)
 DateTime rightNow;  // used to store the current time.
+
+
 
 // Infrared Module
 #include <OneWire.h>
@@ -16,82 +20,111 @@ DateTime rightNow;  // used to store the current time.
 #define IRpin         4
 Adafruit_NECremote remote(IRpin);
 
+
+
 // Traffic Lights - LED Outputs
 #define ledRed A0
-#define ledYellow A1 
+#define ledYellow A1
 #define ledGreen A2
+
+
 
 // DC Motor & Motor Module - L298N
 #include <L298N.h>
-
 // Pin definition
 const unsigned int IN1 = 5;
 const unsigned int IN2 = 6;
-const unsigned int EN = 9;
 
 // Create one motor instance
-L298N motor(EN, IN1, IN2);
+L298N motor(IN1, IN2);
+
+
 
 // Servo
 #include <Servo.h>
 Servo myservo;
 
+
+
 //Potentiometer
 #define pot A3
+
+
 
 // Piezo Buzzer
 #define piezoPin 8
 
+
+
 // Sonar - HC-SR04
 #define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
 #define trigPin A4 //attach pin D3 Arduino to pin Trig of HC-SR04
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
+
 
 // Line Sensor
 #define lineSensorPin 3
 
 int lastcode = -1;
 
+
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void setup() {
   // put your setup code here, to run once:
 
-pinMode(lineSensorPin, OUTPUT);
-Serial.begin(9600);           // Open serial communications and wait for port to open:
+  pinMode(lineSensorPin, OUTPUT);
+  Serial.begin(9600);           // Open serial communications and wait for port to open:
   while (!Serial) {
     delay(1);                   // wait for serial port to connect. Needed for native USB port only
   }
 
-//// SD Card initialisation
-//  Serial.print("Initializing SD card...");
-//  if (!SD.begin(10)) {
-//    Serial.println("initialization failed!");
-//    while (1);
-//  }
-//// Real Time Clock (RTC)
-//  rtc.begin(DateTime(F(__DATE__), F(__TIME__)));
-//  Serial.println("initialization done.");
-//logEvent("System Initialisation...");
 
-// Traffic Lights - LED Outputs
-pinMode(ledRed, OUTPUT);
-pinMode(ledYellow, OUTPUT);
-pinMode(ledGreen, OUTPUT);
 
-// DC Motor & Motor Module - L298N
-motor.setSpeed(70);
+  //// SD Card initialisation
+  //  Serial.print("Initializing SD card...");
+  //  if (!SD.begin(10)) {
+  //    Serial.println("initialization failed!");
+  //    while (1);
+  //  }
+  //// Real Time Clock (RTC)
+  //  rtc.begin(DateTime(F(__DATE__), F(__TIME__)));
+  //  Serial.println("initialization done.");
+  //logEvent("System Initialisation...");
 
-// Servo
+
+
+  // Traffic Lights - LED Outputs
+  pinMode(ledRed, OUTPUT);
+  pinMode(ledYellow, OUTPUT);
+  pinMode(ledGreen, OUTPUT);
+
+
+
+  // DC Motor & Motor Module - L298N
+  pinMode(motorDC, OUTPUT);
+  motor.setSpeed(255);
+
+
+
+  // Servo
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 
-//Potentiometer
-pinMode(pot, INPUT);
 
-// Piezo Buzzer
-pinMode(piezoPin,OUTPUT);
+  //Potentiometer
+  pinMode(pot, INPUT);
 
-// Sonar - HC-SR04
-pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
-pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+
+
+  // Piezo Buzzer
+  pinMode(piezoPin, OUTPUT);
+
+
+
+  // Sonar - HC-SR04
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
 
 
 }
@@ -101,17 +134,54 @@ pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void loop() {
   // put your main code here, to run repeatedly:
-  
 
-remoteDecode();
-SonarSequence();
-LineSensorSequence();
-PotentiometerValue();
-motorDC();
+  Serial.println(readDistance());
+
+
+
+  // myservo.write(0);
+  //  delay(1500);
+  //  myservo.write(90);
+  //  delay(1500);
+  //  myservo.write(180);
+  //  delay(1500);
+
+
+
+  readDistance();
+  remoteDecode();
+  SonarSequence();
+  LineSensorSequence();
+  PotentiometerValue();
+  motorDC();
+  delay(100);
+}
+int readDistance() {
+  // Clears the trigPin condition
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  // Displays the distance on the Serial Monito
+
+  if (distance < 50) {
+    return true;
+    digitalWrite(ledGreen, HIGH);
+  } else {
+    return false;
+    digitalWrite(ledGreen, LOW);
+  }
 }
 
-String remoteDecode() {
-  int c = remote.listen(5);  // seconds to wait before timing out!
+
+void remoteDecode() {
+  int c = remote.listen(1);  // seconds to wait before timing out!
   // Or you can wait 'forever' for a valid code
   //int c = remote.listen();  // Without a #, it means wait forever
   if (c >= 0) {
@@ -120,53 +190,59 @@ String remoteDecode() {
         Serial.println("Code is :" + c);
       // Top keys
       case 70:
-        return "UP";
+        motor.forward();
+        Serial.println("FORWARD");
         break;
       case 21:
-        return"DOWN";
+        motor.backward();
+        Serial.println("DOWN");
         break;
       case 68:
+        myservo.write(180);
+        delay(200);
+        myservo.write(90);
         Serial.println("LEFT");
         break;
       case 67:
+        myservo.write(0);
+        delay(200);
+        myservo.write(90);
         Serial.println("RIGHT");
         break;
       case 64:
-        Serial.println("OK");
+        motor.stop();
+        Serial.println("STOP");
         break;
- 
-}
+
+    }
   }
 }
 
-void motorDC(){
-motor.forward();
-delay(1000);
-motor.stop();
-delay(1000);
-motor.backward();
-delay(1000);
+
+
+void motorDC() {
+  //  motor.forward();
+  //  delay(1000);
+  //  motor.stop();
+  //  delay(1000);
+  //  motor.backward();
+  //  delay(1000);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void RemoteSequence(){
-if (remoteDecode() == "UP")
-}
+void SonarSequence() {
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void SonarSequence(){
 
-  
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void LineSensorSequence(){
+void LineSensorSequence() {
 
-  
+
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void PotentiometerValue() {
 
-  
+
 }
 
 
@@ -183,7 +259,7 @@ void PotentiometerValue() {
 
 
 
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void logEvent(String dataToLog) {
   /*
      Log entries to a file on an SD card.
